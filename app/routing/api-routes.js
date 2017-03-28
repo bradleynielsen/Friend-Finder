d3d769dffb9fe995235f26ba762b8d7d84084e17
Brad Nielsen <BradleyNielsen@gmail.com>
2017-03-28 15:40:14 -0500

    api working ; need to go back and fix bugs

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,13 +1,34 @@
-var friends = require('../data/friends.js');
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on all possible friends
+// ===============================================================================
+
+var friends = require("../data/friends.js");
 
 
-// get the friend array from the datafile
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
 module.exports = function(app) {
 
-  app.get('/api/friends', function (req, res){
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
     res.json(friends);
-    console.log(friends);
-  })
+  });
+
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate Javascript array
+  // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
 
@@ -65,4 +86,5 @@ module.exports = function(app) {
     res.json(bestMatch);
 
   });
+
 };

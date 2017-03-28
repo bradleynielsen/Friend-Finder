5c761ddb50b3ba2f0c9fc37ad6e014bb70a91049
Brad Nielsen <BradleyNielsen@gmail.com>
2017-03-28 16:41:02 -0500

    fixing html padding for home

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,76 +1,51 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on all possible friends
-// ===============================================================================
-
+// load the data from the file containing the friend info
 var friends = require("../data/friends.js");
 
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
 
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
+  // express api call to return the contents of the friends variable, which just returns the json exported in friends.js
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate Javascript array
-  // ---------------------------------------------------------------------------
-
+  // this will update the friends array and compare with the other friends to see what is the best match
   app.post("/api/friends", function(req, res) {
 
-    // Note the code here. Our "server" will respond to a user"s survey result
-    // Then compare those results against every user in the database.
-    // It will then calculate the difference between each of the numbers and the user"s numbers.
-    // It will then choose the user with the least differences as the "best friend match."
-    // In the case of multiple users with the same result it will choose the first match.
-    // After the test, it will push the user to the database.
-
-    // We will use this object to hold the "best match". We will constantly update it as we
-    // loop through all of the options
+    // set defaults for the bestmatch placeholder
     var bestMatch = {
       name: "",
       photo: "",
       friendDifference: 1000
     };
 
-    // Here we take the result of the user"s survey POST and parse it.
+    // grab the request data body and hand it to a variable
     var userData = req.body;
+
+    // grab the scores child from the userdata json
     var userScores = userData.scores;
 
-    // This variable will calculate the difference between the user"s scores and the scores of
-    // each user in the database
+    // init the score for comparing the user and friends
     var totalDifference = 0;
 
-    // Here we loop through all the friend possibilities in the database.
+
+    // start looping over the array of friends held in friends.js
     for (var i = 0; i < friends.length; i++) {
 
+
       console.log(friends[i].name);
+
+      // reset a new score
       totalDifference = 0;
 
-      // We then loop through all the scores of each friend
+      // loop over the scores children, j,   for the current iteration of the loop, i
       for (var j = 0; j < friends[i].scores[j]; j++) {
 
-        // We calculate the difference between the scores and sum them into the totalDifference
+        // build the score from absolute vaulues of the distance away from the user's score away from the firends score
         totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 
-        // If the sum of differences is less then the differences of the current "best match"
         if (totalDifference <= bestMatch.friendDifference) {
 
-          // Reset the bestMatch to be the new friend.
           bestMatch.name = friends[i].name;
           bestMatch.photo = friends[i].photo;
           bestMatch.friendDifference = totalDifference;
@@ -78,11 +53,9 @@ module.exports = function(app) {
       }
     }
 
-    // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-    // the database will always return that the user is the user's best friend).
     friends.push(userData);
 
-    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+    // send back a json for the modal
     res.json(bestMatch);
 
   });

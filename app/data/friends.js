1273fc5931d7ea100b1600e0c452ad53eba60040
Brad Nielsen <BradleyNielsen@gmail.com>
2017-03-28 15:57:36 -0500

    almost all done

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,38 @@ var friendArray = [
       4,
       1
     ]
- }
+ },
+  {
+    "name": "puff",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/a/aa/Papageitaucher_Fratercula_arctica.jpg",
+    "scores": [
+      3,
+      5,
+      2,
+      3,
+      4,
+      3,
+      3,
+      1,
+      3,
+      3
+    ]
+  },
+  {
+    "name": "jeff",
+    "photo": "https://massdrop-s3.imgix.net/product-images/black-diamond-basic-headlamps/MD-33658_20170109115833_f152ee5e731c25da.jpg?auto=format&fm=jpg&fit=crop&w=358&h=358&dpr=1",
+    "scores": [
+      1,
+      5,
+      3,
+      4,
+      5,
+      2,
+      3,
+      1,
+      2,
+      3
+    ]
+  }
 ];
 module.exports = friendArray;

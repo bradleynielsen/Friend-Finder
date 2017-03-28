9ed3e22625d5ad6d2f56ac1d46103129d015a0ef
Brad Nielsen <BradleyNielsen@gmail.com>
2017-03-28 13:50:04 -0500

    working on submit

---
+++ app/data/friend-data.js
@@ -0,0 +1,19 @@
+var friendArray = [
+  {
+   "name":"Ahmed",
+   "photo":"https://upload.wikimedia.org/wikipedia/commons/a/a9/Ahmed_Sultan_Bin_Sulayem.jpg",
+   "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+ }
+];
+module.exports = friendArray;
